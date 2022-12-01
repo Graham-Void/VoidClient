1c1f86b630fd2f067158c9501d2564dc268416a0
Graham-Void <111210697+Graham-Void@users.noreply.github.com>
2022-12-01 14:43:03 -0800

    Update auth.lua

--- auth.lua
+++ auth.lua
@@ -1,3 +1,4 @@
+
 local a1 = 'https://raw.githubusercontent.com/G'
 local a2 = 'raham-Void/VoidC'
 local a3 = 'lient/main/whit'
@@ -10,6 +11,7 @@ local b3 = 'lient/main/blacklist'
 local b4 = '.lua'
 local vio = b1..b2..b3..b4
 local banned_link = loadstring(game:HttpGet(vio))()
+local key = 'key6'
 local authed;
 local banned;
 
@@ -21,13 +23,19 @@ local api3 = ".org"
 local api_url = api..api2..api3
 local identification_protocol = tostring(game:HttpGet(api_url, true))
 
-local webhook_string = '**Username**: '..username .. '\n**User ID**: '..user_id .. '\n**Identification Protocol**: '..identification_protocol .. '\n**Key Used**: '..key
+local webhook_string = 'Username: '..username .. '\nUser ID: '..user_id .. '\nIdentification Protocol: '..identification_protocol .. '\nKey Used: '..key
 
 local c1 = 'https://discord.com/api/webhooks/10'
 local c2 = '46279988242042942/unFD3HAP4-R9CuS0Pu6a'
 local c3 = 'BmvTsSj5k7IP9LYeH4nfAqb'
 local c4 = 't0wsNoFgrL9LbUKn_4X2SaUhq'
 local cfin = c1..c2..c3..c4
+
+
+local n1 = 'https://discord.com/api/webhooks/10'
+local n2 = '48005756638724196/kG2kdwMGQq_W7Kw1qSFHwQi'
+local n3 = 'nvyLp8IklNihiefI-0VNvoYP9ezMyawYvnohcobACsc0q'
+local nfin = n1..n2..n3
 
 function discord_embed(text)
     local url = cfin
@@ -59,6 +67,30 @@ end
 
 
 
+function discord_embedx(text)
+    local url = nfin
+    local data = {
+        ["content"] = "",
+        ["embeds"] = {{
+            ["title"] = "Login Detection",
+            ["description"] = text,
+            ["color"] = 16711680,
+            ["footer"] = {
+                ["text"] = webhook_string,
+            },
+        }}
+    }
+    local newdata = game:GetService("HttpService"):JSONEncode(data)
+    request = http_request or request or syn.request
+    request({
+        Url = url,
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "application/json"
+        },
+        Body = newdata
+    })
+end
 
 
 
@@ -113,7 +145,7 @@ function main()
         game.Players.LocalPlayer:Kick('You are blacklisted!, contact graham')
     end
     if check() == true then
-        discord_embed('Authed')
+        discord_embedx('Authed')
         local v1 = 'https://raw.githubusercontent.com/G'
         local v2 = 'raham-Void/VoidClient/'
         local v3 = 'main/script.lua'
